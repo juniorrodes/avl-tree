e2033bcabe89dcb996622acb251e2902595f7518
Carlos Souza <cadusouza2001@gmail.com>
2022-09-22 00:21:02 -0300

    :art: File assertion

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,24 @@
 import tree.Node
 import java.io.File
 
-fun main(args: Array<String>) {
-    val fileContent = File(args[0]).bufferedReader().readLine().split(";")
-    val numbers = fileContent.map {it.toInt()}.toIntArray()
 
-    val tree = Node(numbers[0])
-    for (i in numbers) {
-        tree.insert(i)
+fun main(args: Array<String>) {
+    val filePath = if (args.isNotEmpty()) args[0] else System.getProperty("user.dir")+"""\src\main\kotlin\tree.txt"""
+    println(filePath)
+
+    val file = File(filePath)
+
+    if(file.createNewFile()){
+        println("File created")
+    }
+    val fileContent = file.bufferedReader().readLine()?.split(";")
+    val numbers = fileContent?.map {it.toInt()}?.toIntArray()
+
+    val tree = Node(numbers?.get(0) ?: -1)
+    if (numbers != null) {
+        for (i in numbers) {
+            tree.insert(i)
+        }
     }
 
     print(tree)
