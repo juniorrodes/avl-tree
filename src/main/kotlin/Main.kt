e62dbf458e0a091c7106c8f7d1b6fc75f49b5666
Carlos Souza <cadusouza2001@gmail.com>
2022-09-23 17:19:41 -0300

    :sparkles: AVL tree auto rebalance feature

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,9 +3,9 @@ import tree.Node
 import java.io.File
 import java.util.Scanner
 
-
+val reader = Scanner(System.`in`)
 fun main(args: Array<String>) {
-    val reader = Scanner(System.`in`)
+    
     val filePath = if (args.isNotEmpty()) args[0] else System.getProperty("user.dir")+"""\src\main\kotlin\tree.txt"""
     
     val file = File(filePath)
@@ -13,8 +13,9 @@ fun main(args: Array<String>) {
         println("File created")
     }
     
-    println("Digite o valor do primeiro nó da árvore:")
-    var tree = Node(reader.nextInt())
+   
+
+    var tree = Node(readInt("Digite o valor do primeiro nó da árvore:"))
 
     while(true){
         println("Selecione uma opção do menu:");
@@ -25,21 +26,21 @@ fun main(args: Array<String>) {
         println("5 - Encaminhamento em pré-ordem")
         println("6 - Encaminhamento em pós-ordem")
         println("7 - Encaminhamento em ordem")
-        println("8 - Carrgar árvore de arquivo")
+        println("8 - Carregar árvore de arquivo")
         println("9 - Salvar árvore em arquivo")
         println("10 - Sair")
         var option = reader.nextInt();
         when(option){
             1 -> BTreePrinter.printNode(tree)
-            2 -> when(tree.find(reader.nextInt())){
+            2 -> when(tree.find(readInt("Digite o valor do nó a ser encontrado:"))){
                 null -> println("Nó não encontrado")
                 else -> println("Nó encontrado")
             }
-            3 -> when(tree.insert(reader.nextInt())){
+            3 -> when(tree.insert(readInt("Digite o valor do nó a ser inserido:"))){
                 null -> println("Nó já existe")
                 else -> println("Nó inserido")
             }
-            4 -> when(tree.delete(reader.nextInt())){
+            4 -> when(tree.delete(readInt("Digite o valor do nó a ser removido:"))){
                 null -> println("Nó não encontrado")
                 else -> println("Nó removido")
             }
@@ -49,6 +50,7 @@ fun main(args: Array<String>) {
             8 -> tree = loadTreeFromFile(filePath)
             9 -> saveTreeToFile(tree, filePath)
             10 -> break;
+            11 -> print(tree)
             else -> println("Opção inválida")
         }
 
@@ -84,4 +86,9 @@ fun loadTreeFromFile(filePath: String): Node {
         }
     }
     return tree
+}
+
+fun readInt(msg: String): Int {
+    println(msg)
+    return reader.nextInt()
 }
--- src/main/kotlin/tree/Node.kt
+++ src/main/kotlin/tree/Node.kt
@@ -1,7 +1,6 @@
 package tree
 
 class Node(private var key: Int,
-           private var level: Int = 0,
            private var height: Int = 1,
            private var balanceFactor: Int = 0,
            private var leftChild: Node? = null,
@@ -29,12 +28,27 @@ class Node(private var key: Int,
         return this.key
     }
 
+    
+
+    fun getBalanceFactor(): Int {
+        return this.balanceFactor
+    }
+
+    fun setLeftChild(node: Node?) {
+        this.leftChild = node
+    }
+
+    fun setRightChild(node: Node?) {
+        this.rightChild = node
+    }
+    
+
     fun insert(value: Int): Node? {
         if (this.find(value) == null) {
             var newNode: Node?
             newNode = if (value < this.key) {
                 if (this.leftChild == null) {
-                    newNode = Node(value, this.level + 1)
+                    newNode = Node(value)
                     this.leftChild = newNode
                     newNode
                 } else {
@@ -42,7 +56,7 @@ class Node(private var key: Int,
                 }
             } else {
                 if (this.rightChild == null) {
-                    newNode = Node(value, this.level + 1)
+                    newNode = Node(value)
                     this.rightChild = newNode
                     newNode
                 } else {
@@ -55,55 +69,6 @@ class Node(private var key: Int,
         } else {
             return null
         }
-    }
-
-    private fun updateBalance() {
-        val leftTreeHeight = this.leftChild?.getHeight() ?: 0
-        val rightTreeHeight = this.rightChild?.getHeight() ?: 0
-
-        this.balanceFactor = leftTreeHeight - rightTreeHeight
-    }
-
-    private fun updateTreeHeight() {
-        val leftTreeHeight = this.leftChild?.getHeight() ?: 0
-        val rightTreeHeight = this.rightChild?.getHeight() ?: 0
-
-        this.height = leftTreeHeight.coerceAtLeast(rightTreeHeight) + 1
-    }
-
-    
-    
-
-    
-
-    override fun equals(other: Any?): Boolean {
-        if (other !is Node) {
-            return false
-        }
-        if (other.rightChild != this.rightChild) {
-            return false
-        }
-        if (other.leftChild != this.leftChild) {
-            return false
-        }
-        if (other.key != this.key) {
-            return false
-        }
-        if (other.level != this.level) {
-            return false
-        }
-        if (other.height != this.height) {
-            return false
-        }
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = key
-        result = 31 * result + height
-        result = 31 * result + (leftChild?.hashCode() ?: 0)
-        result = 31 * result + (rightChild?.hashCode() ?: 0)
-        return result
     }
 
     fun delete(value: Int): Node? {
@@ -122,15 +87,146 @@ class Node(private var key: Int,
             } else if (this.rightChild == null) {
                 this.leftChild
             } else {
-                val minNode = this.rightChild!!.findMin()
-                this.key = minNode!!.key
-                this.rightChild = this.rightChild!!.delete(minNode.key)
+                val maxNode = this.leftChild!!.findMax()
+                this.key = maxNode!!.key
+                this.leftChild = this.leftChild!!.delete(maxNode.key)
                 this
             }
         }
         this.updateBalance()
         this.updateTreeHeight()
         return newNode
+    }
+
+    fun balanceTree(): Node? {
+        if (this.balanceFactor == 2) {
+            if (this.leftChild!!.balanceFactor == -1) {
+                this.leftChild = this.leftChild!!.rotateLeft()
+            }
+
+            return this.rotateRight()
+        } else if (this.balanceFactor == -2) {
+            if (this.rightChild!!.balanceFactor == 1) {
+                this.rightChild = this.rightChild!!.rotateRight()
+            }
+
+            return this.rotateLeft()
+        }
+
+        return this
+    }
+
+    private fun rotateRight(): Node? {
+        val newRoot = this.leftChild
+        this.leftChild = newRoot!!.rightChild
+        newRoot.rightChild = this
+        this.updateBalance()
+        this.updateTreeHeight()
+        newRoot.updateBalance()
+        newRoot.updateTreeHeight()
+
+        return newRoot
+    }
+
+    private fun rotateLeft(): Node? {
+        val newRoot = this.rightChild
+        this.rightChild = newRoot!!.leftChild
+        newRoot.leftChild = this
+        this.updateBalance()
+        this.updateTreeHeight()
+        newRoot.updateBalance()
+        newRoot.updateTreeHeight()
+
+        return newRoot
+    }
+
+    private fun updateBalance() {
+        val leftTreeHeight = this.leftChild?.getHeight() ?: 0
+        val rightTreeHeight = this.rightChild?.getHeight() ?: 0
+
+        this.balanceFactor = leftTreeHeight - rightTreeHeight
+        if(this.balanceFactor >= 2 || this.balanceFactor <= -2) {
+
+
+            val newNode = this.balanceTree()
+            if (newNode != null) {
+                if(newNode.getLeftChild() === this){
+                    this.updateNodeReference(newNode,true)
+                }
+                else if(newNode.getRightChild()===this){
+                    this.updateNodeReference(newNode,false)
+                }
+            }
+
+        }
+    }
+
+    fun duplicateNode(): Node {
+        val newNode = Node(this.key,  this.height, this.balanceFactor)
+        newNode.leftChild = this.leftChild
+        newNode.rightChild = this.rightChild
+        return newNode
+    }
+
+    private fun updateNodeReference(newNode : Node?,equalsLeftNode : Boolean) {
+        var duplicateMe = this.duplicateNode();
+        this.key = newNode!!.getKey()
+        
+        this.height = newNode.getHeight()
+        this.balanceFactor = newNode.getBalanceFactor()
+        if(equalsLeftNode){
+            this.leftChild = duplicateMe
+            this.rightChild = newNode.getRightChild()
+        }
+        else{
+            this.leftChild =  newNode.getLeftChild()
+            this.rightChild = duplicateMe
+        }
+
+
+    }
+
+    private fun updateTreeHeight() {
+        val leftTreeHeight = this.leftChild?.getHeight() ?: 0
+        val rightTreeHeight = this.rightChild?.getHeight() ?: 0
+
+        this.height = leftTreeHeight.coerceAtLeast(rightTreeHeight) + 1
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (other !is Node) {
+            return false
+        }
+        if (other.rightChild != this.rightChild) {
+            return false
+        }
+        if (other.leftChild != this.leftChild) {
+            return false
+        }
+        if (other.key != this.key) {
+            return false
+        }
+       
+        if (other.height != this.height) {
+            return false
+        }
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = key
+        result = 31 * result + height
+        result = 31 * result + (leftChild?.hashCode() ?: 0)
+        result = 31 * result + (rightChild?.hashCode() ?: 0)
+        return result
+    }
+
+    private fun findMax(): Node? {
+        return if (this.rightChild == null) {
+            this
+        } else {
+            this.rightChild!!.findMax()
+        }
     }
 
     private fun findMin(): Node? {
@@ -177,6 +273,9 @@ class Node(private var key: Int,
         return list
     }
 
+    override fun toString(): String {
+        return "Node\n(key=$key, height=$height, balanceFactor=$balanceFactor, leftChild=$leftChild, rightChild=$rightChild)\n"
+    }
 
     
 }
